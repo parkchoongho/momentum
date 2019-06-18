fefc4a41b1f76c8e6b83c086579dce32cd4ea39d
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-06-18 21:34:41 +0900

    API를 활용해 데이터 받아오기
    fetch함수와 API를 활용하여 데이터를 받아오고 then함수를 이용해 필요한 데이터만 뽑아내서 화면에 나타내기

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,29 @@
-const API_KEY = "2b461077b45a03601a15be3110b71617";
-const COORDS = "coords";
+const weatherContainer = document.querySelector(".js-weather");
+
+const API_KEY = "2b461077b45a03601a15be3110b71617",
+  COORDS = "coords";
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      //then이 뜻하는 것은 이 API로 부터 데이터가 다 넘어오면 then이후를 실행하겠다는 뜻이다
+      // 이 API로부터 json 데이터를 받아온다.
+      //console.log(response.json());
+      // reponse를 console에 찍어 보면 network 정보만 보인다.
+      // 이렇게 찍어보면 실질적으로 필요한 데이터는 pending되고 있는 것으로 나온다. 따라서 다음 코드에 then을 한번 더 찍어준다.
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+
+      const location = json.name;
+      const temp = Math.floor(json.main.temp);
+
+      weatherContainer.innerText = `${location}동, ${temp}도 `;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -14,6 +38,7 @@ function handleGeoSuccess(position) {
     lon
   };
   saveCoords(coordsObj);
+  getWeather(lat, lon);
 }
 
 function handleGeoError() {
@@ -29,7 +54,9 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
-    //getWeather
+    const parsedCoords = JSON.parse(loadedCoords);
+    // console.log(typeof parsedCoords.lat);
+    getWeather(parsedCoords.lat, parsedCoords.lon);
   }
 }
 
