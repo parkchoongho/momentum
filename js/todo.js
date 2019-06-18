c3e33dffb0fc5aad7ec4df6e4ecb82bdf4f7afed
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-06-18 15:49:10 +0900

    toDo 리스트 local storage에 저장하고 화면에 뿌리기
    local storage에서 데이터를 받아오고 새로고침을 눌러도 데이터가 화면에 뿌려지는 것이 반영되게 하기

--- js/todo.js
+++ js/todo.js
@@ -4,20 +4,47 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODO_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODO_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
-    const li = document.
+  const li = document.createElement("li"),
+    delBtn = document.createElement("button"),
+    span = document.createElement("span"),
+    newId = toDos.length + 1,
+    toDoObj = {
+      text: text,
+      id: newId
+    };
+  toDos.push(toDoObj);
+  delBtn.innerText = "❌";
+  span.innerText = text;
+  li.id = newId;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+  saveToDos();
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
-  paintToDo(value);
+  paintToDo(currentValue);
+  toDoInput.value = "";
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODO_LS);
-  //   if (toDos !== null) {
-  //   }
+  const loadedToDos = localStorage.getItem(TODO_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      console.log(toDo.text);
+      paintToDo(toDo.text);
+    });
+  }
 }
 
 function init() {
